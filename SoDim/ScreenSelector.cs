60152640e84eb82f9071ac5a2e25de084fb62160
SpitfireX <timm.weber@me.com>
2016-05-29 03:18:38 +0200

    ScreenSelector renders every screen configuration now correctly.

--- SoDim/ScreenSelector.cs
+++ SoDim/ScreenSelector.cs
@@ -22,33 +22,55 @@ namespace SoDim
             int width = this.Size.Width,
                 height = this.Size.Height,
                 workspaceWidth = 0,
-                workspaceHeight = 0;
+                workspaceHeight = 0,
+                maxX = 0,
+                minX = 0,
+                maxY = 0,
+                minY = 0;
 
             foreach (var screen in Screen.AllScreens)
             {
                 Rectangle b = screen.Bounds;
                 Debug.WriteLine(b);
 
-                if (b.X + b.Width > workspaceWidth)
-                    workspaceWidth = b.X + b.Width;
+                if (b.X < minX)
+                    minX = b.X;
+                if (b.X + b.Width > maxX)
+                    maxX = b.X + b.Width;
 
-                if (b.Y + b.Height > workspaceHeight)
-                    workspaceHeight = b.Y + b.Height;
+                if (b.Y < minY)
+                    minY = b.Y;
+                if (b.Y + b.Height > maxY)
+                    maxY = b.Y + b.Height;
             }
+            workspaceWidth = maxX - minX;
+            workspaceHeight = maxY - minY;
+
             Debug.WriteLine(workspaceWidth + ", " + workspaceHeight);
 
-
-
-            double scale = 0.0;
+            //scaling factor to fit the workspace into the viewport
+            double scale = 0.0,
+                   invscale = 0.0;
             if (workspaceWidth > workspaceHeight)
             {
-                scale = (double) width / workspaceWidth;
+                scale = (double)width / workspaceWidth;
+                invscale = (double)workspaceWidth / width;
             }
             else
             {
-                scale = (double) height / workspaceHeight;
+                scale = (double)height / workspaceHeight;
+                invscale = (double)workspaceHeight / height;
             }
-            Debug.WriteLine("scale: " + scale);
+            Debug.WriteLine("scale: {0}, invscale: {1}", scale, invscale);
+
+            //offset to only get positive coordinates
+            int offsetX = 0,
+                offsetY = 0;
+            
+            if (minX < 0)
+                offsetX = 0 - minX;
+            if (minY < 0)
+                offsetY = 0 - minY;
 
             int count = 1;
             foreach (var screen in Screen.AllScreens)
@@ -56,8 +78,8 @@ namespace SoDim
                 Rectangle b = screen.Bounds;
                 int sbWidth = (int)(b.Width * scale);
                 int sbHeight = (int)(b.Height * scale);
-                int sbX = (int)(b.X * scale);
-                int sbY = (int)(b.Y * scale);
+                int sbX = (int)((b.X + offsetX) * scale);
+                int sbY = (int)((b.Y + offsetY) * scale);
                 Debug.WriteLine(sbX + ", " + sbY + ", " + sbWidth + ", " + sbHeight);
 
                 ScreenButton sb = new ScreenButton(new Size(sbWidth, sbHeight), count.ToString(), screen.DeviceName, "0%");
